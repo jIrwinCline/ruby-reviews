7aeefd48d80c67722f06fe0a06dc339dafee917d
Jacob Irwin <jakobinwins@gmail.com>
2019-08-09 15:05:02 -0700

    addviews for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,46 +1,46 @@
 class ProductsController < ApplicationController
   def index
-    @albums = Album.all
+    @products = Product.all
     render :index
   end
   def new
-    @album = Album.new
+    @product = Product.new
     render :new
   end
   def create
-    @album = Album.new(album_params)
-    if @album.save
-      redirect_to albums_path
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
     else
       render :new
     end
 
     private
-    def album_params
-      params.require(:album).permit(:name, :genre)
+    def product_params
+      params.require(:product).permit(:name, :genre)
     end
   end
 
   def edit
-    @album = Album.find(params[:id])
+    @product = Product.find(params[:id])
     render :edit
   end
 
   def show
-    @album = Album.find(params[:id])
+    @product = Product.find(params[:id])
     render :show
   end
   def update
-    @album= Album.find(params[:id])
-    if @album.update(album_params)
-      redirect_to albums_path
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
     else
       render :edit
     end
   end
   def destroy
-    @album = Album.find(params[:id])
-    @album.destroy
-    redirect_to albums_path
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,55 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review successfully created!"
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    flash[:notice] = "Review successfully edited!"
+    render :edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "Review successfully updated!"
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "Review successfully destroyed!"
+    redirect_to product_path(@review.product)
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:user_name, :text)
+    end
+
+end
