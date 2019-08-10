09ae51d158787ece888d79ce8d08d0e49a1dbf25
Jacob Irwin <jakobinwins@gmail.com>
2019-08-09 19:24:40 -0700

    finished most validations and began error messages

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
+  validates :rubies, length: { in: 1..5 }
+  validates :user_name, presence: true
+  validates :text, length: { minimum: 5 }
   belongs_to :product
 end
