11eaa2afb1f28f81755abdb35c80ebafab99ed08
Jacob Irwin <jakobinwins@gmail.com>
2019-08-16 15:25:17 -0700

    add integration spec to products page spec

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -19,4 +19,31 @@ describe "the add a product process" do
     click_on 'Create Product'
     expect(page).to have_content "Name can't be blank"
   end
+  it "checks out a product that was made" do
+    visit products_path
+    click_link 'Add a new product for Review'
+    fill_in 'Name', :with => 'Ford Truck'
+    fill_in 'Company', :with => 'Company'
+    fill_in 'Purpose', :with => 'This product has a purpose'
+    fill_in 'Cost', :with => '11.98'
+    fill_in 'Country of origin', :with => 'USA'
+    click_on 'Create Product'
+    click_on 'Ford Truck'
+    expect(page).to have_content 'Ford Truck'
+  end
+  it "Edits a product" do
+    visit products_path
+    click_link 'Add a new product for Review'
+    fill_in 'Name', :with => 'Ford Truck'
+    fill_in 'Company', :with => 'Company'
+    fill_in 'Purpose', :with => 'This product has a purpose'
+    fill_in 'Cost', :with => '11.98'
+    fill_in 'Country of origin', :with => 'USA'
+    click_on 'Create Product'
+    click_on 'Ford Truck'
+    click_on 'Edit'
+    fill_in 'Name', :with => 'Chevy Truck'
+    click_on 'Update Product'
+    expect(page).to have_content 'Chevy Truck'
+  end
 end
